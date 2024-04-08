6859b861c31691d68acf5d691a5f52b6befb53c1
Sanindu Rathnayake <rathnayakesanindu99@gmail.com>
2024-04-08 01:28:31 +0100

    Changed the strategy of fee calculation and added testing
    Changed the strategy of fee calculation. Created two new methods and added testing for those new discount() and dimensionWeight() methods.

--- src/group05/Parcel.java
+++ src/group05/Parcel.java
@@ -2,7 +2,7 @@ package group05;
 
 public class Parcel {
 
-    private String id; 
+    private String id;
     private double length;
     private double width;
     private double height;
@@ -79,50 +79,67 @@ public class Parcel {
     public void setDestination(String destination) {
         this.destination = destination;
     }
-    
-    public int parcelFee(double length, double width, double weight, int noOfDays, int discounts) {
-    	int totalFee;
-    	int dimentionFare;
-    	int StorageDurationFee;
-    	String parcelType;
-    	
 
-    	if (weight ==0 || length == 0 || width == 0 ) {
-    		System.out.println("Invalid Dimension");
-    		return -1;
-    	}
-    	else if (weight <= Constants.SMALL_WEIGHT_LIMIT && length <= Constants.SMALL_MAX_LENGTH 
-    			 && width<= Constants.SMALL_MAX_WIDTH) {
-    		parcelType = "SMALL";
-    		dimentionFare = Constants.SMALL_FARE;
-    		System.out.println("Small Parcel");
-    	}
-    	else if (weight <= Constants.MEDIUM_WEIGHT_LIMIT && length <= Constants.MEDIUM_MAX_LENGTH 
-   			 && width<= Constants.MEDIUM_MAX_WIDTH) {
-    		parcelType = "MEDIUM";
-    		dimentionFare = Constants.MEDIUM_FARE;
-    		System.out.println("Medium Parcel");
-    	}
-    	//No Width limitation for Large Parcels
-    	else if (weight <= Constants.LARGE_WEIGHT_LIMIT && length <= Constants.LARGE_MAX_LENGTH) {
-    		parcelType = "LARGE";
-    		dimentionFare = Constants.LARGE_FARE;
-    		System.out.println("Large Parcel");
-    	}
-    	else {
-    		System.out.println("Sorry! Out of Range Parcel Dimension");
-    		return -1;
-    	}
+    public double parcelFee(int length, int width, int height, int weight, int noOfDays) {
+        double totalFee;
+        double dimensionFare;
+        double StorageDurationFee;
+        double discounts;
+        String parcelType;
+        int dimensionalWeight;
 
-    	// Fee Calculation for Number of Days in the Deport
-    	if(noOfDays == 2) {
-    		StorageDurationFee = 0;
-    	}
-        else StorageDurationFee = Constants.PER_DAY_RATE*noOfDays;
-    		
-    	totalFee = Constants.BASE_FARE + dimentionFare + StorageDurationFee - discounts;
-    	
-    	return totalFee;
-    	
+        dimensionalWeight = dimensionWeight(length, width, height);
+        // Selects the greater of the two weights (actual weight or dimensional weight) as the chargeable weight
+        if (dimensionalWeight != 0 && dimensionalWeight > weight) {
+            weight = dimensionalWeight;
+        }
+        if (weight <= Constants.SMALL_WEIGHT_LIMIT) {
+            parcelType = "SMALL";
+            dimensionFare = Constants.SMALL_FARE;
+        }
+        else if (weight <= Constants.MEDIUM_WEIGHT_LIMIT) {
+            parcelType = "MEDIUM";
+            dimensionFare = Constants.MEDIUM_FARE;
+        }
+        else if (weight <= Constants.LARGE_WEIGHT_LIMIT) {
+            parcelType = "LARGE";
+            dimensionFare = Constants.LARGE_FARE;
+        } else {
+            System.out.println("Sorry! Out of Range Parcel Dimension");
+            return -1;
+        }
+
+        // Fee Calculation for Number of Days in the Deport
+        if (noOfDays == 2) {
+            StorageDurationFee = 0;
+        } else StorageDurationFee = Constants.PER_DAY_RATE * noOfDays;
+        discounts = discount(parcelType, dimensionFare);
+        totalFee = Constants.BASE_FARE + dimensionFare + StorageDurationFee - discounts;
+        System.out.println("Parcel Type : " + parcelType);
+        String formattedFee = String.format("%.2f", totalFee);
+        System.out.println("Parcel Total Fee : GBP " + formattedFee);
+        return totalFee;
+    }
+
+    // There will be a discount for the dimension fare if the parcel type is Large
+    public double discount(String parcelType, double dimensionFare) {
+        double discount;
+        if (parcelType.equals("LARGE")) {
+            discount = Constants.DISCOUNT_RATE * dimensionFare;
+        } else discount = 0.0;
+        return discount;
+    }
+
+    public int dimensionWeight(int length, int width, int height) {
+        double dimensionalWeight;
+        //Calculate Dimensional Weight: Divide the volume in cubic meters by the carrier's volumetric factor.
+        // The result is the dimensional weight of the package in kilograms (kg).
+        try {
+            dimensionalWeight = (length * width * height) / (1000000 * Constants.VOLUMETRIC_FACTOR);
+            return (int) dimensionalWeight;
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e);
+            return 0;
+        }
     }
 }
